bb8a97c0f4698ceb4e9363f797291d0f0221fc21
oriharaa <qstar9537@naver.com>
2021-03-14 23:54:53 +0900

    Implementing slides

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import NavbarItem from '../navbarItem/navbarItem';
 import styles from './header.module.css';
 import headerData from '../../data/header.json';
@@ -7,8 +7,27 @@ import Slider from '../slider/slider';
 
 const Header = (props) => {
 
+  const headerRef = useRef();
+
+  useEffect(()=> {
+    slide();
+  });
+
+  const slide = () => {
+    setInterval(()=> {
+      headerRef.current.style.background = 'url("http://www.miero.co.kr/img/main/banner2.jpg")';
+      headerRef.current.style.animationName = 'dd';
+      headerRef.current.style.animationIterationCount = '3';
+      headerRef.current.style.animationFillMode = 'both';
+      headerRef.current.style.animationDelay = '1s';
+      console.log(headerRef.current.style.animation);
+    }, 3000)
+  };
+
+
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} ref={headerRef}>
       <nav className={styles.navbar}>
         <h1>
           <a>
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './slider.module.css';
+import headerData from '../../data/header.json';
 
-const Slider = (props) => (
-  <div className={styles.slider}></div>  
-);
+const Slider = (props) => {
+
+
+  return (
+  <div className={styles.slider}>
+    <div className={styles.info}>
+      <h3 className={styles.title}>
+        마시는 식이섬유<br/>미에로화이바
+      </h3>
+      {headerData.sns.map(sns => 
+        <a 
+          className={styles.sns}
+          target="_blank"
+          href={sns.link}
+        >
+          <i className={sns.icon}></i>
+        </a>
+      )}
+    </div>
+    <div className={styles.move}>
+      <button className={styles.btn}>
+        <img src="/images/header/left.png"></img>
+      </button>
+      <button className={styles.btn}>
+        <img src="images/header/right.png"></img>
+      </button>
+    </div>
+    <ul className={styles.tabs}>
+      <li>
+        <a 
+          className={styles.tab}
+          href="#"
+        ></a>
+      </li>
+    </ul>
+  </div>  
+)};
 
 export default Slider;
